177030b79f5d12056a99d1eab9d8ec8e11598ae3
Sawera Malik <saweram693@gmail.com>
2025-06-26 14:29:31 +0500

    make some changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-slate-800 p-4 " style={{ padding: 30, fontFamily: "sans-serif" }}>
       <h1 className="text-4xl font-bold mb-5">Home</h1>
       <a href="/login" target="_blank" className="text-blue-500 underline"> Go to Login Page</a>
-      <a href="/login" target="_blank" className="text-blue-500 underline"> Go to Card Page</a>
 
     </div>
   );
--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,9 +1,10 @@
 import { LoaderFunction, redirect } from "@remix-run/node";
 import { destroySession, getSession } from "../routes/session.server";
 
-// Helper function
 async function logoutAndRedirect(request: Request) {
     const session = await getSession(request.headers.get("Cookie"));
+    console.log('>>> logout', request.headers);
+
     return redirect("/login", {
         headers: {
             "Set-Cookie": await destroySession(session),
@@ -11,7 +12,6 @@ async function logoutAndRedirect(request: Request) {
     });
 }
 
-// Remix loader
 export const loader: LoaderFunction = async ({ request }) => {
     return logoutAndRedirect(request);
 };
