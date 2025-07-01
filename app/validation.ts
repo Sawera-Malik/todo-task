9071247ea589a79fa9ec0a8e0a408856134e8c51
Sawera-Malik <saweram993@gmail.com>
2025-07-01 19:30:48 +0500

    update code structure

--- app/validation.ts
+++ app/validation.ts
@@ -1,12 +1,12 @@
 import { withZod } from "@remix-validated-form/with-zod";
 import { z } from "zod";
 
-export const validatorLogin = withZod(z.object({
+export const loginValidator = withZod(z.object({
     email: z.string().min(1, { message: "Email is required" }).email(" Must be a valid email"),
     password: z.string().min(6, "Password must be at least 6 characters"),
 }));
 
-export const validatorTodo = withZod(z.object({
+export const todoValidator = withZod(z.object({
     text: z.string().min(1, { message: "Text is required" }),
     body: z.string().min(1, { message: "Body is required" }),
 }))
